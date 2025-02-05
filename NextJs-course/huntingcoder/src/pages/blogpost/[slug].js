dc84b60d670ea3b925f60cb9e27ca7f171528486
moradiyaparth-cp <moradiyaparth6660@gmail.com>
2025-02-05 09:31:52 +0530

    complete video 15 creating blog and blogpost page

--- NextJs-course/huntingcoder/src/pages/blogpost/[slug].js
+++ NextJs-course/huntingcoder/src/pages/blogpost/[slug].js
@@ -7,11 +7,16 @@ const slug = () => {
     console.log(router.query)
     const {slug} = router.query
   return (
-    <div>
-      <h1>Title of the page: {slug}</h1>
-      <div>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Commodi, a veritatis. Velit voluptatibus dolorem ipsam consequatur quo vel perspiciatis repudiandae mollitia veniam eius!
-      </div>
+    <div className={styles.container}>
+      <main className={styles.main}>
+        <h1>Title of the page: {slug}</h1>
+        <div>
+          <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Commodi, a veritatis. Velit voluptatibus dolorem ipsam consequatur quo vel perspiciatis repudiandae mollitia veniam eius! 
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut aperiam nesciunt nostrum quam ratione, optio, id illum atque eaque dolorem cumque magni laboriosam cum quaerat adipisci, voluptatibus dolor voluptates soluta? Ea incidunt vel vero quae recusandae! Impedit doloremque voluptatem voluptatum ipsum delectus dolore mollitia distinctio.
+          </p>
+        </div>
+      </main>
     </div>
   )
 }
