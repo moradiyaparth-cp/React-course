6f2811975763f675f0445c6c76289efd85b971ba
moradiyaparth-cp <moradiyaparth6660@gmail.com>
2025-02-05 12:50:48 +0530

    complete video 19 using api endpoint

--- NextJs-course/huntingcoder/src/pages/blog.js
+++ NextJs-course/huntingcoder/src/pages/blog.js
@@ -1,27 +1,31 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import styles from "@/styles/Blog.module.css";
 import Link from 'next/link';
 
 const Blog = () => {
+    const [blogs, setBlogs] = useState([])
+    useEffect(() => {
+      console.log("useeffect is runnnig")
+      fetch("http://localhost:3000/api/blogs")
+      .then((a)=>{ return a.json(); })
+        .then((parsed)=>{
+            console.log(parsed)
+            setBlogs(parsed)
+        })
+      }, [])
+ 
+    
   return (
     <div className={styles.container}>
         <main className={styles.main}>
 
-            <div className={styles.blogItem}>
-            <Link href={'/blogpost/learn-jacascript'}>
-                <h3>How to learn JavaScript in 2025?</h3></Link>
-                <p>JavaScript is a language used to design logic for the web</p>
+        {blogs.map((blogitem)=>{
+            return <div className={styles.blogItem} key={blogitem.title}>
+            <Link href={`/blogpost/${blogitem.slug}`}>
+                <h3>{blogitem.title}</h3></Link>
+                <p>{blogitem.content.substr(0, 140)}...</p>
             </div>
-
-            <div className={styles.blogItem}>
-                <h3>How to learn JavaScript in 2025?</h3>
-                <p>JavaScript is a language used to design logic for the web</p>
-            </div>
-
-            <div className={styles.blogItem}>
-                <h3>How to learn JavaScript in 2025?</h3>
-                <p>JavaScript is a language used to design logic for the web</p>
-            </div>
+        })}
 
         </main>
   </div>
