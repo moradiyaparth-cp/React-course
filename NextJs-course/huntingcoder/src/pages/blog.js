9ce319b7e7fcf4e387fb7bdd1decfb5ad5f114bf
moradiyaparth-cp <moradiyaparth6660@gmail.com>
2025-02-05 16:45:18 +0530

    complete video 22 server side rendering

--- NextJs-course/huntingcoder/src/pages/blog.js
+++ NextJs-course/huntingcoder/src/pages/blog.js
@@ -2,19 +2,13 @@ import React, {useEffect, useState} from 'react'
 import styles from "@/styles/Blog.module.css";
 import Link from 'next/link';
 
-const Blog = () => {
-    const [blogs, setBlogs] = useState([])
-    useEffect(() => {
-      console.log("useeffect is runnnig")
-      fetch("http://localhost:3000/api/blogs")
-      .then((a)=>{ return a.json(); })
-        .then((parsed)=>{
-            console.log(parsed)
-            setBlogs(parsed)
-        })
-      }, [])
+const Blog = (props) => {
+    console.log(props)
+    const [blogs, setBlogs] = useState(props.allBlogs)
+    // useEffect(() => {
+      
+    //   }, [])
  
-    
   return (
     <div className={styles.container}>
         <main className={styles.main}>
@@ -32,4 +26,12 @@ const Blog = () => {
   )
 }
 
+export async function getServerSideProps(context) {
+    let data = await fetch("http://localhost:3000/api/blogs")
+    let allBlogs = await data.json()
+
+    return{
+        props: {allBlogs}
+    }
+}
 export default Blog
--- NextJs-course/huntingcoder/src/pages/blogpost/[slug].js
+++ NextJs-course/huntingcoder/src/pages/blogpost/[slug].js
@@ -1,19 +1,17 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { useRouter } from 'next/router'
 import styles from "@/styles/BlogPost.module.css";
 
-const slug = () => {
-    const router = useRouter();
-    console.log(router.query)
-    const {slug} = router.query
+const slug = (props) => {
+  const [blog, setBlog] = useState(props.myBlog)
+  
   return (
     <div className={styles.container}>
       <main className={styles.main}>
-        <h1>Title of the page: {slug}</h1>
+        <h1>{blog && blog.title}</h1>
         <div>
           <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Commodi, a veritatis. Velit voluptatibus dolorem ipsam consequatur quo vel perspiciatis repudiandae mollitia veniam eius! 
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut aperiam nesciunt nostrum quam ratione, optio, id illum atque eaque dolorem cumque magni laboriosam cum quaerat adipisci, voluptatibus dolor voluptates soluta? Ea incidunt vel vero quae recusandae! Impedit doloremque voluptatem voluptatum ipsum delectus dolore mollitia distinctio.
+          {blog && blog.content}
           </p>
         </div>
       </main>
@@ -21,4 +19,14 @@ const slug = () => {
   )
 }
 
+export async function getServerSideProps(context) {
+    const {slug} = context.query
+    
+    let data = await fetch(`http://localhost:3000/api/getblog?slug=${slug}`)
+    let myBlog = await data.json()
+ 
+  return{
+      props: {myBlog}
+  }
+}
 export default slug
