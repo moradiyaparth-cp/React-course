934d5a9737f561ca8e7c2cbe53aa19d906ab7dc2
moradiyaparth-cp <moradiyaparth6660@gmail.com>
2025-02-07 18:21:51 +0530

    create mini e-commerce project using nextjs

--- NextJs-course/e-store/src/app/page.js
+++ NextJs-course/e-store/src/app/page.js
@@ -1,10 +1,15 @@
+import FeaturedProduct from '@/components/home/FeaturedProduct'
+import HeroSection from '@/components/home/HeroSection'
+import RecentlyAdded from '@/components/home/RecentlyAdded'
 import React from 'react'
 
 const Home = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <HeroSection />
+      <RecentlyAdded />
+      <FeaturedProduct />
+    </>
   )
 }
 
--- NextJs-course/e-store/src/components/home/HeroSection.js
+++ NextJs-course/e-store/src/components/home/HeroSection.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import Slider from './Slider'
+import Container from '../Container'
+import styles from '@/styles/home/hero.module.css'
 
 const HeroSection = () => {
   return (
-    <div>HeroSection</div>
+    <Container>
+      <section className={styles.hero}>
+        {/* Text section  */}
+        <div className={`${styles.textSection} text-gray-700`}>
+          <h1>One stop solution <span className='text-pink-500'>E-Store</span></h1>
+          <p>Discover the latest headphones, earphones, mobiles, tablets etc.</p>
+          <p>Exclusive deals just for you!</p>
+          <button className={styles.ctaButton}>Shop Now</button>
+        </div>
+      <Slider />
+      </section>
+    </Container>
   )
 }
 
