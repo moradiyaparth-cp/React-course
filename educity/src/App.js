9ff7fd2d40be919a4b7640d30df691188957c2a8
moradiyaparth-cp <moradiyaparth6660@gmail.com>
2025-02-19 18:00:15 +0530

    create educity project

--- educity/src/App.js
+++ educity/src/App.js
@@ -2,6 +2,8 @@ import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
 import Hero from './Components/Hero/Hero'
 import Programs from './Components/Programs/Programs'
+import Title from './Components/Title/Title'
+import About from './Components/About/About'
 
 const App = () => {
   return (
@@ -9,8 +11,9 @@ const App = () => {
       <Navbar />
       <Hero />
       <div className="container">
+        <Title subTitle='Our PROGRAM' title='What We Offer'/>
         <Programs />
-
+        <About />
       </div>
     </div>
   )
