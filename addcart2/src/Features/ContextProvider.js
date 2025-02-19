279677c886ce5fbaa1ac851906c4abb981af9ba2
moradiyaparth-cp <moradiyaparth6660@gmail.com>
2025-02-19 14:47:47 +0530

    create add to cart feature

--- addcart2/src/Features/ContextProvider.js
+++ addcart2/src/Features/ContextProvider.js
@@ -1,7 +1,7 @@
 import React, { createContext, useReducer } from 'react';
 import CartReducer from './CartReducer';
 
-export const CartContext = createContext(); // data share kare ane provider return kare jethi data che e child component access kari shake, store create thay
+export const CartContext = createContext(); //store create thay, data share kare ane provider return kare jethi data che e child component access kari shake
 
 const ContextProvider = ({ children }) => {
 
@@ -13,6 +13,8 @@ const ContextProvider = ({ children }) => {
   const [cart, dispatch] = useReducer(CartReducer, [], storeData); // dispatch -> CartReducer ne action send kare,  [] -> initial value che cart ni, storeData -> ani value CartReducer mathi avshe ane store thase
 
   return (
+    
+    // {/* store create karelo che eni value badha page ne mali re e mate Provider ni help thi store pass karavi didho */}
     <CartContext.Provider value={{ cart, dispatch }}>
       {children}
     </CartContext.Provider>
