2e8ef1929504f32184a6314e34c2ac2882020b6e
moradiyaparth-cp <moradiyaparth6660@gmail.com>
2025-02-20 18:19:16 +0530

    create all section responsive

--- educity/src/App.js
+++ educity/src/App.js
@@ -4,6 +4,11 @@ import Hero from './Components/Hero/Hero'
 import Programs from './Components/Programs/Programs'
 import Title from './Components/Title/Title'
 import About from './Components/About/About'
+import Campus from './Components/Campus/Campus'
+import Testimonials from './Components/Testimonials/Testimonials'
+import Contact from './Components/Contact/Contact'
+import Footer from './Components/Footer/Footer'
+import VideoPlayer from './Components/VideoPlayer/VideoPlayer'
 
 const App = () => {
   return (
@@ -11,10 +16,24 @@ const App = () => {
       <Navbar />
       <Hero />
       <div className="container">
+
         <Title subTitle='Our PROGRAM' title='What We Offer'/>
         <Programs />
+
         <About />
+
+        <Title subTitle='Gallery' title='Campus Photos'/>
+        <Campus />
+
+        <Title subTitle='TESTIMONIALS' title='What Student Says'/>
+        <Testimonials />
+
+        <Title subTitle='Contact Us' title='Get in Touch'/>
+        <Contact />
+
+        <Footer />
       </div>
+      <VideoPlayer />
     </div>
   )
 }
